814add06ac8a526c869de5610e7a0a41566eb8d2
clairvoya1998 <weixiaoyanelsa@gmail.com>
2018-11-24 21:26:22 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	scrumMaster.py

--- scrumMaster.py
+++ scrumMaster.py
@@ -6,7 +6,7 @@ import pickle
 import boto3
 
 from flask import Flask
-from flask_ask import Ask, request, session, question, statement
+from flask_ask import Ask, request, session, question, statement, delegate
 
 
 app = Flask(__name__)
@@ -23,6 +23,14 @@ raw_data = obj['Body'].read()
 archieve_info = pickle.loads(raw_data)
 
 
+
+standup_meeting = False
+
+team_size = 3
+team_counter = 0
+team_members = ['a', 'b', 'c']
+
+
 @ask.launch
 def launch():
     speech_text = "Hey guys. Here I am, Alexa, your Scrum Master. What's your project?"
@@ -32,6 +40,7 @@ def launch():
 def get_new_project():
     speech_text = 'Please confirm the name of your new project.'
     return question(speech_text).reprompt(speech_text)
+
 
 @ask.intent('ProjectNameIntent')
 def new_project(Text):
@@ -43,6 +52,86 @@ def new_project(Text):
         speech_text = 'You said: {}, what a great name, shall we start the sprint?'.format(Text)
         # add Text to the array
     return question(speech_text).reprompt(speech_text)
+
+
+def get_name():
+    if (team_counter != team_size):
+        temp = team_members[team_counter]
+        name_counter = team_counter + 1
+        return temp
+
+
+#STAND UP MEETING
+#Alexa, where's my scrum master
+#Hello, what's your project
+#projectName
+#IF NOT END OF SPRINT
+#i THINK it's time for a stand up meeting
+
+def get_dialog_state():
+    return session['dialogState']
+
+@ask.intent('StandUpMeetingIntent')
+def start_stand_up():
+
+    dialog_state = get_dialog_state()
+    if dialog_state != "COMPLETED":
+        return delegate()
+
+    for x in range(0, team_size):
+        attendance(x)
+    #standup_meeting = True
+    speech_text = "Great. It's time for the daily stand up. Let's take attendance."
+    return question(speech_text).reprompt(speech_text).simple_card('Attendance', speech_text)
+
+def take_attendance():
+    for i in range(team_size):
+        attendance(i)
+    return 0
+
+@ask.intent('AMAZON.YesIntent')
+def yes_intent():
+    return statement("Hi")
+
+
+@ask.intent('AMAZON.NoIntent')
+def no_intent():
+    return statement("What a shame.")
+
+@ask.intent("AttendanceIntent")
+def attendance(i):
+    speech_text = "<p>{}?</p>".format(team_members[i])
+
+    # check user says "Here"
+    # else "Oh no. user is not here."
+    return question(speech_text)
+
+
+@ask.intent('SprintDateIntent')
+def sprint_update():
+    days_left = 5 # sprint end date - current date
+    tasks_left = 5
+    return question('There are' + days_left + 'days left in the sprint and' + tasks_left + 'tasks left to accomplish')
+
+
+def ask_team_about_work():
+    for i in range(team_size):
+        ask_about_yesterday(i)
+        ask_about_today(i)
+    return 0
+
+
+@ask.intent('AskAboutYesterdayIntent')
+def ask_about_yesterday(i):
+    speech_text = format(team_members[i]) + 'what did you do yesterday?'
+    return question(speech_text)
+
+
+@ask.intent('AskAboutTodayIntent')
+def ask_about_today(i):
+    speech_text = format(team_members[i]) + 'what are you going to do today?'
+    return question(speech_text)
+
 
 
 
