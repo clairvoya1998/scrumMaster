fcdce0c5eeea62b86b2f407935d95850d8efd16f
--unset <vibe96@gmail.com>
2018-11-25 09:52:37 +0000

    Upgrade email handling

--- scrumEmail.py
+++ scrumEmail.py
@@ -17,12 +17,13 @@ def readmail():
         sys.exit(1)
     print("Logged in")
 
-    mail.select('inbox', readonly=1)
+    mail.select('inbox')
     result, data = mail.search(None, '(UNSEEN)')
     mail_ids = data[0]
     id_list = mail_ids.split()
-    
+
     for i in id_list:
+        mail.store(i, '+FLAGS', '\Seen')
         result, data = mail.fetch(i, "(RFC822)")
         raw_email = data[0][1].decode("utf-8")
         #print(raw_email)
@@ -40,21 +41,4 @@ def get_first_text_block(email_message_instance):
         return email_message_instance.get_payload()
 
 
-
-    # first_email_id = int(id_list[0])
-    # latest_email_id = int(id_list[-1])
-    #
-    # print("Before loop")
-    # for i in range(latest_email_id, first_email_id, -1):
-    #     print("In loop")
-    #     typ, data = mail.fetch(i, '(RFC822)' )
-    #
-    #     for response_part in messages:
-    #         if isinstance(response_part, tuple):
-    #             msg = email.message_from_string(response_part[1])
-    #             email_subject = msg['subject']
-    #             email_from = msg['from']
-    #             print('From : ' + email_from + '\n')
-    #             print('Subject : ' + email_subject + '\n')
-
-readmail()
+# readmail()
